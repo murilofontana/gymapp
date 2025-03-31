cfa40873a38b673da6485eccfedbad5dfc318d7f
Murilo Fontana <fontana.murilo@gmail.com>
2025-03-31 20:59:43 -0300

    factory to create timerange

--- GymApp.Domain/Common/ValueObjects/TimeRange.cs
+++ GymApp.Domain/Common/ValueObjects/TimeRange.cs
@@ -15,6 +15,22 @@ public class TimeRange : ValueObject
 
     public static ErrorOr<TimeRange> FromDateTimes(DateTime start, DateTime end)
     {
+        if (start.Date != end.Date)
+        {
+            return Error.Validation(description: "Star and end date times must be the same date");
+        }
+
+        if (start >= end)
+        {
+            return Error.Validation(description: "End time must be greater than start time");
+        }
+
+        return new TimeRange(TimeOnly.FromDateTime(start), TimeOnly.FromDateTime(end));
+
+    }
+
+    public static ErrorOr<TimeRange> FromDateTimes(DateTime start, DateTime end)
+    {
         if (start.Date != end.Date || start >= end)
         {
             return Error.Validation();
