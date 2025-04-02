25759563cd4d2a1f0ec5cd7aee6bc09ece22b8a7
Murilo Fontana <fontana.murilo@gmail.com>
2025-04-02 19:36:38 -0300

    repository

---
+++ GymApp.Application/Common/ISessionsRepository.cs
@@ -0,0 +1,9 @@
+﻿using GymApp.Domain.SessionAggregate;
+
+namespace GymApp.Domain.Common.Interfaces;
+
+public interface ISessionsRepository
+{
+    Task AddSessionAsync(Session session);
+    Task UpdateSessionAsync(Session session);
+}
